3b64ca9a371ba09fe1ede35d28319cacbb3049b9
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2015-12-01 15:55:16 +0100

    Basic interface + outlets and actions

--- Restos/ViewController.swift
+++ Restos/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var styleSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var alreadyVisitedSwitch: UISwitch!
+    @IBOutlet weak var gradeSlider: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func saveRestaurant(sender: UIButton) {
+    }
 
 }
 
